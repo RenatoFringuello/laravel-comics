f6bf3ddc57f8960f7aa785f8b5087b6d8696c8dd
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-09 13:19:04 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <div class="row">
+        <div class="col-12 text-danger">hello</div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,55 @@
+<header class="sans-narrow">
+    <div class="container">
+        <div class="img-container">
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </div>
+        <nav class="nav-links">
+            <ul>
+                <li class="active">
+                    <a href="#">CHARACTERS</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">COMICS</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">MOVIES</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">TV</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">GAMES</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">COLLECTIBLES</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">VIDEOS</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">FANS</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">NEWS</a>
+                    <div class="active-bar"></div>
+                </li>
+                <li class="">
+                    <a href="#">SHOP</a>
+                    <div class="active-bar"></div>
+                </li>
+            </ul>
+        </nav>
+        <div class="search-container">
+            <input type="text" name="searchBar" id="search-bar" placeholder="Search">
+            {{-- search icon --}}
+        </div>
+    </div>
+</header>
