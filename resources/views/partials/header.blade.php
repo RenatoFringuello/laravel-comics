3b74965be7362f7d907c517999b78c00572fa47f
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-10 20:28:28 +0100

    add header-info

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,55 +1,68 @@
-<header class="sans-narrow">
-    <div class="container">
-        <div class="img-container">
-            <img src="{{ Vite::asset('./resources/assets/images/dc-logo.png')}}" alt="DC Logo">
-        </div>
-        <nav class="nav-links">
-            <ul>
-                <li class="active">
-                    <a href="#">CHARACTERS</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">COMICS</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">MOVIES</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">TV</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">GAMES</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">COLLECTIBLES</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">VIDEOS</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">FANS</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">NEWS</a>
-                    <div class="active-bar"></div>
-                </li>
-                <li class="">
-                    <a href="#">SHOP</a>
-                    <div class="active-bar"></div>
-                </li>
-            </ul>
-        </nav>
-        <div class="search-container">
-            <input type="text" name="searchBar" id="search-bar" placeholder="Search">
-            <i class="fa-solid fa-magnifying-glass"></i>
+<header>
+    <div class="header-info-wrapper">
+        <div class="container">
+            <span>DC POWER™VISA&reg;</span>
+            <select name="dc-sites" id="dc-sites">
+                <option value="1">Additional DC Sites</option>
+                <option value="2">B</option>
+                <option value="3">C</option>
+            </select>
         </div>
     </div>
+    <div class="header-wrapper sans-narrow">
+        <div class="container">
+            <div class="img-container">
+                <img src="{{ Vite::asset('./resources/assets/images/dc-logo.png')}}" alt="DC Logo">
+            </div>
+            <nav class="nav-links">
+                <ul>
+                    <li class="active">
+                        <a href="#">CHARACTERS</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">COMICS</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">MOVIES</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">TV</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">GAMES</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">COLLECTIBLES</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">VIDEOS</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">FANS</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">NEWS</a>
+                        <div class="active-bar"></div>
+                    </li>
+                    <li class="">
+                        <a href="#">SHOP</a>
+                        <div class="active-bar"></div>
+                    </li>
+                </ul>
+            </nav>
+            <div class="search-container">
+                <input type="text" name="searchBar" id="search-bar" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+    </div>
+
 </header>
