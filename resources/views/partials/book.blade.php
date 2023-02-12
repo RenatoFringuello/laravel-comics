f180d33b2abe52bdedc3f7cfee0f4150f6e3b8eb
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-12 11:38:12 +0100

    add a link to the comic route

--- resources/views/partials/book.blade.php
+++ resources/views/partials/book.blade.php
@@ -1,7 +1,7 @@
-<div class="book">
+<a href="/comic/" class="book">
     <div class="img-container">
         <img src="{{$comic['thumb']}} alt="{{$comic['series']}}">
     </div>
     <div class="text">{{ $comic['series'] }}</div>
     <div class="text">{{ $comic['price'] }}</div>
-</div>
+</a>
