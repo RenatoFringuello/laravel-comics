e246f2cabd10c969cd505f17c34774c81128b436
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-13 18:53:09 +0100

    add show

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -50,7 +50,8 @@ class ComicController extends Controller
      */
     public function show($id)
     {
-        //
+        $comic = Comic::find($id);
+        return view('comic', compact('comic'));
     }
 
     /**
