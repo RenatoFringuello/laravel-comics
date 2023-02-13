8920512c7be3b6a6da4cd2cfcdf2242e6429f670
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-13 18:55:15 +0100

    add comic thumbnail show

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -1,5 +1,13 @@
 <div class="jumbotron">
-    <div class="container">
-        <div class="sans-narrow filled">current series</div>
-    </div>
+    @if (isset($thumb))
+        <div class="filled">
+            <div class="comic-container">
+                <img class="comic-img img-fluid" src="{{$thumb}}" alt="">
+            </div>
+        </div>
+    @else
+        <div class="container">
+            <div class="sans-narrow filled">current series</div>
+        </div>
+    @endif
 </div>
