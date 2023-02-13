442948360533f234d30fabd6b3cc3f38bde7150b
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-13 18:54:45 +0100

    add a route on book click to cover

--- resources/views/partials/book.blade.php
+++ resources/views/partials/book.blade.php
@@ -1,6 +1,6 @@
-<a href="/comic/" class="book">
+<a href="{{route('comic', $comic)}}" class="book">
     <div class="img-container">
-        <img src="{{$comic['thumb']}} alt="{{$comic['series']}}">
+        <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
     </div>
     <div class="text">{{ $comic['series'] }}</div>
     <div class="text">{{ $comic['price'] }}</div>
