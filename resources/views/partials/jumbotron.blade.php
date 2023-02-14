e8d2786e40015f7c4a1e120412629d725a4d5485
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-14 15:12:40 +0100

    ad style to jumbotron img

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -2,7 +2,11 @@
     @if (isset($thumb))
         <div class="filled">
             <div class="comic-container">
-                <img class="comic-img img-fluid" src="{{$thumb}}" alt="">
+                <div class="wrapper">
+                    <div class="info top-0 text-uppercase">Comic Book</div>
+                    <div class="info bottom-0 w-100 text-center text-uppercase">View Gallery</div>
+                    <img class="comic-img img-fluid" src="{{$thumb}}" alt="">
+                </div>
             </div>
         </div>
     @else
