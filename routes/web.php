d089c83b81f03dba53d5a4ebe1079295ad9e4ded
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-16 00:46:41 +0100

    add delete edit update routes

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', [ComicController::class, 'index'])->name('home');
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::post('/comics/store', [ComicController::class, 'store'])->name('comics.store');
+Route::get('/comics/{id}/edit', [ComicController::class, 'edit'])->name('comics.edit');
+Route::put('/comics/{comic}/update', [ComicController::class, 'update'])->name('comics.update');
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
+Route::delete('/comics/delete/{id}', [ComicController::class, 'destroy'])->name('comics.delete');
