07860e4b2d324e2cf9f0c9011b10e2645c8060c0
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-16 16:11:25 +0100

    add validate fn to validate in update & store

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -8,6 +8,46 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
+
+    /**
+     * validate the form data before save them into the db
+     *
+     * @param Request $request
+     * @return void
+     */
+    private function validateData(Request $request){
+        $request->validate(
+            [
+                'title' => 'required|max:100',
+                'description' => 'required',
+                'thumb' => 'required|url|',
+                //prima metto numeric per validare che non sia una stringa poi con decimal verifico il numero di cifre decimali
+                'price' => 'required|numeric|decimal:0,2',
+                'series' => 'required|max:255',
+                //|after_or_equal:today crea problemi con la edit
+                'sale_date' => 'required|date',
+                'type' => 'required|max:255',
+            ],
+            [
+                'title.required' => 'Il titolo è un campo obbligatorio',
+                'title.max' => 'Hai inserito troppi caratteri in title',
+                'description.required' => 'La descrizione è un campo obbligatorio',
+                'thumb.required' => 'Il path dell\'immagine è un campo obbligatorio',
+                'thumb.url' => 'Il path dell\'immagine è deve essere un indirizzo valido',
+                'price.required' => 'Il prezzo è un campo obbligatorio',
+                'price.decimal' => 'Hai inserito troppe cifre decimali',
+                'price.numeric' => 'Il prezzo è un numero non qualsiasi cosa tu abbia scritto (usa il punto per i decimali)',
+                'series.required' => 'Il titolo della saga è un campo obbligatorio',
+                'series.max' => 'Hai inserito troppi caratteri in series',
+                'sale_date.required' => 'La data di uscita è un campo obbligatorio',
+                'sale_date.date' => 'La data di uscita che hai scritto non esiste in nessun calendario neanche in quello dei maya',
+                // 'sale_date.after_or_equal' => 'La data di uscita non può essere stata nel passato dal momento che sta uscendo oggi o uscirà in futuro',
+                'type.required' => 'Il tipo è un campo obbligatorio',
+                'type.max' => 'Hai inserito troppi caratteri in type',
+            ]
+        );
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -42,35 +82,8 @@ class ComicController extends Controller
     {
         $data = $request->all();
         //dd($data);
-        $request->validate(
-            [
-                'title' => 'required|max:100',
-                'description' => 'required',
-                'thumb' => 'required|url|',
-                //prima metto numeric per validare che non sia una stringa poi con decimal verifico il numero di cifre decimali
-                'price' => 'required|numeric|decimal:0,2',
-                'series' => 'required|max:255',
-                'sale_date' => 'required|date|after_or_equal:today',
-                'type' => 'required|max:255',
-            ],
-            [
-                'title.required' => 'Il titolo è un campo obbligatorio',
-                'title.max' => 'Hai inserito troppi caratteri in title',
-                'description.required' => 'La descrizione è un campo obbligatorio',
-                'thumb.required' => 'Il path dell\'immagine è un campo obbligatorio',
-                'thumb.url' => 'Il path dell\'immagine è deve essere un indirizzo valido',
-                'price.required' => 'Il prezzo è un campo obbligatorio',
-                'price.decimal' => 'Hai inserito troppe cifre decimali',
-                'price.numeric' => 'Il prezzo è un numero non qualsiasi cosa tu abbia scritto (usa il punto per i decimali)',
-                'series.required' => 'Il titolo della saga è un campo obbligatorio',
-                'series.max' => 'Hai inserito troppi caratteri in series',
-                'sale_date.required' => 'La data di uscita è un campo obbligatorio',
-                'sale_date.date' => 'La data di uscita che hai scritto non esiste in nessun calendario neanche in quello dei maya',
-                'sale_date.after_or_equal' => 'La data di uscita non può essere stata nel passato dal momento che sta uscendo oggi o uscirà in futuro',
-                'type.required' => 'Il tipo è un campo obbligatorio',
-                'type.max' => 'Hai inserito troppi caratteri in type',
-            ]
-        );
+        $this->validateData($request);
+        
         $newComic = new Comic();
         $newComic->title = $data['title'];
         $newComic->description = $data['description'];
@@ -120,6 +133,8 @@ class ComicController extends Controller
     {
         $data = $request->all();
         //dd($data);
+        $this->validateData($request);
+
         $comic->title = $data['title'];
         $comic->description = $data['description'];
         $comic->thumb = $data['thumb'];
